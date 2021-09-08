929668c548d300fdb5cecf47a50f1d168d4bbfcc
venkana831 <obulasetti2000@gmail.com>
2021-09-08 12:33:06 +0530

    save

---
+++ src/test/java/com/vaadin/tutorial/addressbook/AddressbookUITest.java
@@ -0,0 +1,24 @@
+package com.vaadin.tutorial.addressbook;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AddressbookUITest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testInitVaadinRequest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRefreshContacts() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/vaadin/tutorial/addressbook/ContactFormTest.java
@@ -0,0 +1,39 @@
+package com.vaadin.tutorial.addressbook;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class ContactFormTest {
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testContactForm() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCancel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEdit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUI() {
+		fail("Not yet implemented");
+	}
+
+}
